d47995d2eabf9649c9b5170acfb30cab2d9f8d76
rbrickmn <rbrickmn@gmail.com>
2024-07-11 10:36:02 -0400

    Begin post container

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -6,11 +6,15 @@ const Feed = () => {
     <div className="feed">
       <div className="post-card">
         <div className="card-wrapper">
-          <div className="pic-container"></div>
-          <div className="subreddit-name">
-            <span className="bold-name">r/subredditname &middot;</span>{" "}
-            <span className="time-posted">11hr. ago</span>
+          <div className="post-details">
+            <div className="pic-container"></div>
+            <p>
+              <span className="bold-name">r/subredditname &middot;</span>{" "}
+              <small className="time-posted">11hr ago</small>
+            </p>
           </div>
+
+          <h2 className="post-title">Test Heading This is a longer bit of text to see how far this card will stretch upon reaching the breakpoint. How is it doing?</h2>
         </div>
       </div>
     </div>
