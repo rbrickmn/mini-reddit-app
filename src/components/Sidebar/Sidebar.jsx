d3cec98f0234783066bd876433467fab0f8df8cc
rbrickmn <rbrickmn@gmail.com>
2024-07-13 12:11:23 -0400

    Update sidebar to include images and nav buttons

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,21 +4,38 @@ import "./Sidebar.css";
 const Sidebar = () => {
   return (
     <div className="sidebar-wrapper">
+      <div className="nav-wrapper">
+        <div className="nav-btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#433bff"
+          >
+            <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+          </svg>
+          <p className="nav-text">Home</p>
+        </div>
+      </div>
+
       <div className="subreddits-container">
+        <small className="sub-title">SUBREDDITS</small>
         <ul>
           <li>
+            <div className="nav-pic-container"></div>
             <p>r/subredditname</p>
           </li>
           <li>
+            <div className="nav-pic-container"></div>
             <p>r/subredditname</p>
           </li>
           <li>
+            <div className="nav-pic-container"></div>
             <p>r/subredditname</p>
           </li>
           <li>
-            <p>r/subredditname</p>
-          </li>
-          <li>
+            <div className="nav-pic-container"></div>
             <p>r/subredditname</p>
           </li>
         </ul>
